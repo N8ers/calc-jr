5bf701eddf9c0afb845c732d6f0d73a5e1fff46e
N8ers <nathanraysheryak@gmail.com>
2018-07-09 08:38:48 -0400

    I'm sleepy and need to go on a walk

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-$('.btn').click(function(){
-    $('.output').val($(this).val());
+// $('.btn').click(function(event){
+//     var thingClicked = this.innerHTML
+//     console.log("you clicked: ", thingClicked)
+//         $(".output").html(thingClicked)
+// });
+
+// var btnClick = 
+// document.querySelector(".btn");
+// btnClick.addEventListener("click", function(){
+//     document.querySelector(".output").innerHTML = ".btn".val();
+//     concole.log(".btn".val());
+// });
+
+var btnClick = document.getElementById("#btn");
+
+btnClick.addEventListener("click", function(){
+    console.log("clicked!"); 
 })
